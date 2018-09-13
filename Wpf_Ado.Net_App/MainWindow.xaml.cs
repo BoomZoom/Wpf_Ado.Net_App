04f9acf8bd72462f1c212483f67df904a18082ef
Andrey <andrey.venzhega@gmai.com>
2018-09-13 15:29:13 +0300

    binding need update

--- Wpf_Ado.Net_App/MainWindow.xaml.cs
+++ Wpf_Ado.Net_App/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Wpf_Ado.Net_App
 {
     /// <summary>
@@ -20,9 +21,36 @@ namespace Wpf_Ado.Net_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*
+         Пример:
+
+        form = form ?? new FormClass();
+
+        Эту строку можно расписать как:
+
+        form = form != null ? form : new FormClass();
+        */
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new Wpf_AdoViewModel();
+
+            //айяяй
+            // создаем привязку команды
+            CommandBinding commandBinding = new CommandBinding();
+            // устанавливаем команду
+            commandBinding.Command = ApplicationCommands.Help;
+            // устанавливаем метод, который будет выполняться при вызове команды
+            commandBinding.Executed += CommandBinding_Executed;
+            // добавляем привязку к коллекции привязок элемента Button
+            helpButton.CommandBindings.Add(commandBinding);
+            //---------------------------------------------
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Справка по приложению");
         }
     }
 }
---
+++ Wpf_Ado.Net_App/Wpf_AdoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace Wpf_Ado.Net_App
+{
+    internal class Wpf_AdoViewModel:INotifyPropertyChanged
+    {
+        public Wpf_AdoViewModel()
+        {
+            page = "Page2.xaml";
+        }
+
+        private string page;
+        public string Page
+        {
+            get { return page; }
+            set
+            {
+                page = value;
+                OnPropertyChanged("Page");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
